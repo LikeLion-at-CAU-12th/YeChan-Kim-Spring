f8a584a33012e8c5be5c2d5e92f1c34fe5e7375c
roganyc <roganyc@naver.com>
2024-09-05 09:38:49 +0900

    [chore] 초기 세팅

---
+++ src/test/java/com/example/likelionspring/LikelionspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
