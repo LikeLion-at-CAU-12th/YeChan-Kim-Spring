e46cff3da1ca55ce55268bc0b57ef7848e27487a
roganyc <roganyc@naver.com>
2024-10-02 19:22:40 +0900

    [chore] 4주차 세션 진행중

---
+++ src/main/java/com/example/likelionspring/dto/request/ArticleCreateRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.likelionspring.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ArticleCreateRequestDto {
+    private Long id;
+    private String title;
+    private String content;
+    private List<Long> categoryIds;
+}
